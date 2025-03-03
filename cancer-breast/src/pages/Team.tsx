1409b710635bae1b92d5e9565d0fafa3411555b9
Miguel Zavala <miguel@mikkezavala.com>
2025-03-03 00:00:47 -0800

    update site

--- cancer-breast/src/pages/Team.tsx
+++ cancer-breast/src/pages/Team.tsx
@@ -1,11 +1,15 @@
 import {Avatar, Card, Col, Row} from "antd";
 
+import mza from '/src/assets/mz.png'
+import bb from '/src/assets/bb.png'
+import ba from '/src/assets/ba.png'
+
 export const Team = () => {
     return (
         <Row gutter={16}>
             <Col span={8}>
                 <Card variant="borderless">
-                    <Card.Meta avatar={<Avatar src="/src/assets/mz.png"/>} title="Miguel Zavala"/>
+                    <Card.Meta avatar={<Avatar src={mza}/>} title="Miguel Zavala"/>
                     <p>
                         I am a passionate software engineer. I've spent more than two decades writing code for
                         enterprises in a set of diverse industries. I started my career as a software engineer in travel
@@ -20,7 +24,7 @@ export const Team = () => {
             </Col>
             <Col span={8}>
                 <Card variant="borderless">
-                    <Card.Meta avatar={<Avatar src="/src/assets/bb.png"/>} title="Brittany Blackmon"/>
+                    <Card.Meta avatar={<Avatar src={bb}/>} title="Brittany Blackmon"/>
                     <p>
                         Brittany Blackmon is a Dallas, TX resident working in global lifecycle marketing at Peloton,
                         where she specializes in building brand loyalty and engagement. Her role integrates her
@@ -44,20 +48,20 @@ export const Team = () => {
             </Col>
             <Col span={8}>
                 <Card variant="borderless">
-                    <Card.Meta avatar={<Avatar src="/src/assets/ba.png"/>} title="Blake Armstrong"/>
+                    <Card.Meta avatar={<Avatar src={ba}/>} title="Blake Armstrong"/>
                     <p>
                         Blake is an engineer with over seven years of experience in the oil and gas industry, currently
                         contributing to a Fort Worth-based private operator. Throughout my career, I have held various
                         roles, demonstrating a strong understanding of both technical and operational aspects of the
                         sector. Alongside my engineering career, I love to test my entrepreneurial spirit with real
                         estate investments.
-                        <br />
+                        <br/>
                         I graduated from Texas A&M University in 2017 with a BS in Petroleum Engineering and a Petroleum
                         Ventures Program certification. As part of the inaugural class of the Petroleum Ventures
                         Program, students received a unique education opportunity that blends petroleum engineering with
                         finance, providing them with a well-rounded foundation in both technical and financial
                         disciplines.
-                        <br />
+                        <br/>
                         In my free time, I enjoy spending time with my chocolate lab, Boudreaux. If I have some extra
                         free time I also enjoy hanging out with friends, playing golf, and ideally both at the same
                         time.
